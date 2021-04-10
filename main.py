f2569a9b4989e1afce92f98b7b61f94323d3cbb7
giorgoshar <34591362+giorgoshar@users.noreply.github.com>
2021-04-11 02:17:29 +0300

    -- Remaking the irc bot with less libraries needed

--- main.py
+++ main.py
@@ -1,194 +1,105 @@
-import os
-import time
 import sys
-import asyncio
-import re
-import threading
-import unicodedata
-import random
-import imp
-import ctypes
-from twisted.words.protocols import irc
-from twisted.internet import reactor, protocol, ssl, task
-from twisted.python import log
-from config import Config
+import socket
+import time
 
+class User:
+    def __init__(self, username, hostname, servername, realname):
+        self.username   = username
+        self.hostname   = hostname
+        self.servername = servername
+        self.realname   = realname
 
-class MessageLogger:
-    def __init__(self, file):
-        self.file = file
+    def __repr__(self):
+        return f'''
+            \rusername: {self.username}
+            \rhostname: {self.hostname}
+            \rsrvname : {self.servername}
+            \rrealname: {self.realname}
+        '''
 
-    def log(self, message):
-        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
-        self.file.write('%s %s\n' % (timestamp, message))
-        self.file.flush()
-
-    def close(self):
-        self.file.close()
-
-class ModuleManager:
-    def __init__(self, bot):
-        self.modules = []
-        self.bot     = bot
-        self.path    = os.getcwd() + '/modules'
-        sys.path.append(self.path)
-        self.load()
-
-
-    def run(self, data):
-        threads = []
-        for module in self.modules:
-            if module['enabled'] == False: continue
-            t = threading.Thread(target=module['run'], args=(self.bot, data,))
-            t.setDaemon(True)
-            t.start()
-            threads.append(t)
-        
-        # for module in self.modules:
-        #    d = threads.deferToThread(module['run'], self.bot, data)
-
-    def load(self):
-        print ("Loading modules")
-        self.modules = []
-        for item in os.scandir(self.path):
-            if item.is_file():
-                _, ext = os.path.splitext(self.path + '/' + item.name)
-                if ext == '.py':
-                    name = item.name
-                    mod  = imp.load_source(name, _ + ext)
-                    if hasattr(mod, 'setup'): mod.setup(self.bot)
-                    module = {
-                        'name'    : name,
-                        'mod'     : mod,
-                        'run'     : mod.run   if hasattr(mod, 'run'  ) else None,
-                        'enabled' : True,
-                    }
-                    self.modules.append(module)
-                    print ('Module `%s` has been loaded' % (name))
-    
-    def reload(self):
-        safe_threads = ['MainThread', 'PoolThread-twisted.internet.reactor-0']
-        for thread in threading.enumerate():
-            if thread.name in safe_threads:
-                continue
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(thread.ident), ctypes.py_object(SystemExit))
-        self.load()
-
-    def list(self):
-        mods = []
-        for module in self.modules:
-            mod = {
-                'name'    : module['name'],
-                'enabled' : module['enabled']
-            }
-            mods.append(mod)
-        return str(mods)
-
-    def disable(self, name):
-        for module in self.modules:
-            if module['name'] == name:
-                module['enabled'] = False
-
-    def enable(self, name):
-        for module in self.modules:
-            if module['name'] == name:
-                module['enabled'] = True
-
-class LogBot(irc.IRCClient):
-    nickname = 'scibot'
-    password = ''
-    lineRate = None
-
-    def __init__(self):
-        irc.IRCClient.__init__(self)
-        self.mainchan = '#testing'
-        self.modules  = ModuleManager(self)
-        self.raw_data = ''
-
-    def connectionMade(self):
-        # Connect to server
-        irc.IRCClient.connectionMade(self)
-        print ("Connected")
-        self.logger = MessageLogger(open(self.factory.filename, "a"))
-        self.logger.log("[Connected at %s]" % time.asctime(time.localtime(time.time())))
-
-    def connectionLost(self, reason):
-        irc.IRCClient.connectionLost(self, reason)
-        self.logger.log("[Disconnected at %s]" % time.asctime(time.localtime(time.time())))
-        self.logger.close()
-
-    def signedOn(self):
-        self.join(self.factory.channel)
-
-    def joined(self, channel):
-        self.logger.log("[I have joined %s]" % self.factory.channel)
-
-    def privmsg(self, user, channel, msg):
-
-        user = user.split('!', 1)[0]
-        self.logger.log("<%s> %s" % (user, msg))
-
-        data = {
-            'event'   : 'privmsg',
-            'user'    : user,
-            'channel' : channel,
-            'msg'     : msg,
-            'raw_data': self.raw_data
-        }
-
-        self.modules.run(data)
-
-        if(msg.startswith('!reload') and user == 'Singularity'):
-            self.modules.reload()
-
-        if channel == self.nickname: pass # prive
-        if channel.startswith('#'):  pass # channel
-
-    def alterCollidedNick(self, nickname):
-        return nickname + str(random.randint(0, 100))
-
-    def action(self, user, channel, msg):
-        user = user.split('!', 1)[0]
-        self.logger.log("* %s %s" % (user, msg))
-
-    def irc_NICK(self, prefix, params):
-        old_nick = prefix.split('!')[0]
-        new_nick = params[0]
-        self.logger.log("%s is now known as %s" % (old_nick, new_nick))
-    
-    def userJoined(self, user, channel):
-        self.logger.log("%s has joined %s" % (user, channel))
-
-    def dataReceived(self, data):
-        self.raw_data = data
-        irc.IRCClient.dataReceived(self, data)
-
-class LogBotFactory(protocol.ClientFactory):
-    protocol = LogBot
-    def __init__(self, channel, filename):
-        self.channel  = channel
+class Logger:
+    def __init__(self, filename):
         self.filename = filename
-    # def startedConnecting(self, connector):
-        # print ('Connection stared', connector)
-    def clientConnectionLost(self, connector, reason):
-        print ("Lost connection (%s), reconnecting." % (reason))
-        # connector.connect()
-        sys.exit()
-    def clientConnectionFailed(self, connector, reason):
-        print ("Connection failed:", reason)
-        reactor.stop()
+        self.file = open(filename, 'a')
+    def write(self, message):
+        now = time.asctime()
+        self.file.write(f'[{now}]: {message}\n')
 
-class Bot(LogBotFactory):
+class IRC:
+    CRLF = '\r\n'
     def __init__(self):
-        LogBotFactory.__init__(self, ','.join(Config.channels), Config.logfile)
+        self.socket = None
+        self.userinfo = User('myusername','myhostname','myservername','myrealname')
+        self.nickname = 'myfirstircconnection'
+        self.channels = ['#testingircconnect']
 
-    def connect(self, hostname, port):
-        reactor.connectTCP(Config.hostname, Config.port, self)
-        # reactor.connectSSL(hostname, port, f, ssl.ClientContextFactory())
-        reactor.run()
+        self.logger = open('log', 'a')
 
-if __name__ == '__main__':
-    log.startLogging(sys.stdout)
-    bot = Bot()
+    def connect(self, host, port):
 
-    bot.connect(Config.hostname, Config.port)
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((host, port))
+
+            #NICK <nickname>
+            nick  = f'NICK {self.nickname}'
+            self.send(nick)
+
+            #USER <username> <hostname> <servername> <realname>
+            uname = f'USER {self.userinfo.username} {self.userinfo.hostname} {self.userinfo.servername} {self.userinfo.realname}'
+            self.send(uname)
+
+            for chan in self.channels:
+                channel = f'JOIN {chan}'
+                self.send(channel)
+
+            message = (f'PRIVMSG  #testingircconnect :mymessage')
+            self.send(message)
+
+            running = True
+            while running:
+                recv = self.socket.recv(4096).decode('utf-8')
+                self.recv(recv)
+
+        except Exception as e:
+            self.socket.close()
+            print(f'{host}:{port} Failed', e)
+            sys.exit(1)
+
+    def send(self, text, encoding='utf-8'):
+        message = (text + IRC.CRLF).encode(encoding)
+        self.socket.send(message)
+        print(f'[{time.asctime()}][LEN:{len(message):>06}]: {message}', end='')
+
+    def recv(self, messages):
+
+        if len(messages) > 0:
+            self.logger.write(messages)
+
+        print(f'[{time.asctime()}][LEN:{len(messages):>06}]: {messages}', end='')
+        messages = messages.split('\n')
+        for message in messages:
+            if '!kill' in message:
+                self.socket.close()
+                sys.exit()
+            if '!say' in message:
+                self.irc_PRIVMSG('#testingircconnect', 'response!~')
+
+
+    def irc_NICK(self, nickname):
+        self.send(f'NICK {nickname}')
+    def irc_USER(self, userinfo):
+        self.send(f'USER {userinfo.username} {userinfo.hostname} {userinfo.servername} {userinfo.realname}')
+    def irc_JOIN(self, channel):
+        self.send(f'JOIN {channel}')
+    def irc_PRIVMSG(self, to, message):
+        self.send(f'PRIVMSG {to} :{message}')
+
+
+irc = IRC()
+irc.connect('chat.freenode.net', 6667)
+
+# Userinfo = User('myusername','myhostname','myservername','myrealname')
+
+# print(Userinfo)
+# print(repr(Userinfo))
